6ab09e2ec50a47ebdcb7e8cc626a42a1822f79e5
sengdu <sengdu005416@gmail.com>
2021-04-07 18:15:24 +0630

    react-bootstrap

---
+++ src/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const About = () => (
+	<div>
+		<h2>About</h2>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, rem eaque quibusdam atque, corporis cupiditate asperiores nisi doloribus fuga a cum dolores, est in. Labore quaerat asperiores dignissimos tempore nisi.</p>
+	</div>
+);
